a9252ceb847449632f438102fc0b6d8165d07ad5
confitee <adsc112406@gmail.com>
2026-06-01 15:54:06 -0600

    feat: add hero component and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { counterPageComponent } from './pages/counter/counter.component';
+import { HeroPageComponent } from './pages/hero/hero-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'hero',
+        component: HeroPageComponent
+    }
+];
---
+++ src/app/pages/hero/hero-page.component.ts
@@ -0,0 +1,48 @@
+import { Component, signal } from "@angular/core";
+
+@Component({
+    templateUrl: './hero-page.component.html',
+    styleUrl: './hero-page.component.css'
+})
+
+export class HeroPageComponent {
+    public name = signal('Iron Man');
+    public age = signal(45);
+
+    getHeroDescription(): string {
+    return this.name() + ' - ' + this.age() + ' años';
+}
+
+cambiarHero(): void {
+
+    if (this.name() === 'Iron Man') {
+        this.name.set('Spider-Man');
+        
+    } 
+    else if (this.name() === 'Spider-Man') {
+        this.name.set('Dr. Strange');
+    } 
+    else {
+        this.name.set('Iron Man');
+    }
+}
+cambiarEdad(): void{
+    if (this.age() === 45){
+        this.age.set(20);
+    }
+    else if (this.age() === 20){
+        this.age.set(40);
+    }
+    
+    else {
+        this.age.set(45);
+    }
+
+}
+    
+
+    resetForm(): void {
+        this.name.set('Iron Man');
+        this.age.set(45);
+    }
+}
